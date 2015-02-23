6a9f3ae30690d298c1b6444a811f25cfff2b1cee
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-02-23 21:05:28 +0000

    curl -u params

--- webExamples/modules/users/api.php
+++ webExamples/modules/users/api.php
@@ -1,22 +1,22 @@
 <?php
-//api $ curl http://polar-dawn-5134.herokuapp.com/api?module=users -X POST -d '{"key":"val"}' -H 'accept:application/json'
-//auth $ curl http://user:pass@polar-dawn-5134.herokuapp.com/auth?module=users -X POST -d '{"key":"val"}' -H 'accept:application/json'
+//api $ curl http://domain.com/api?module=users -X POST -d '{"key":"val"}' -H 'accept:application/json'
+//auth $ curl -u user:pass http://domain.com/auth?module=users -X POST -d '{"key":"val"}' -H 'accept:application/json'
 Api::post(function($req, $res){
     $res->json([
         'input' => $req->input()
     ]);
 });
 
-//api $ curl http://polar-dawn-5134.herokuapp.com/api?module=users -H 'accept:application/json'
-//auth $ curl http://user:pass@polar-dawn-5134.herokuapp.com/auth?module=users -H 'accept:application/json'
+//api $ curl http://domain.com/api?module=users -H 'accept:application/json'
+//auth $ curl -u user:pass http://domain.com/auth?module=users -H 'accept:application/json'
 Api::get(function($req, $res){
     $res->json([['id' => 1, 'name' => 'pete', 'job' => 'security'],
         ['id' => 2,'name' => 'John', 'job' => 'care taker'],
         ['id' => 3,'name' => 'jane', 'job' => 'assistant']]);
 });
 
-//api $ curl http://polar-dawn-5134.herokuapp.com/api?module=users -X PUT -d '{"key":"val"}' -H 'accept:application/json'
-//auth $ curl http://user:pass@polar-dawn-5134.herokuapp.com/auth?module=users -X PUT -d '{"key":"val"}' -H 'accept:application/json'
+//api $ curl http://domain.com/api?module=users -X PUT -d '{"key":"val"}' -H 'accept:application/json'
+//auth $ curl -u user:pass http://domain.com/auth?module=users -X PUT -d '{"key":"val"}' -H 'accept:application/json'
 Api::put(function($req, $res){
     $res->json([
         'verb' => $req->verb,
@@ -24,8 +24,8 @@ Api::put(function($req, $res){
     ]);
 });
 
-//api $ curl http://polar-dawn-5134.herokuapp.com/api?module=users?id=2 -H 'accept:application/json'
-//auth $ curl http://user:pass@polar-dawn-5134.herokuapp.com/auth?module=users?id=2 -H 'accept:application/json'
+//api $ curl http://domain.com/api?module=users?id=2 -H 'accept:application/json'
+//auth $ curl -u user:pass http://domain.com/auth?module=users?id=2 -H 'accept:application/json'
 Api::delete(function($req, $res){
     $res->json([
         'verb' => $req->verb,
@@ -33,8 +33,8 @@ Api::delete(function($req, $res){
     ]);
 });
 
-//api $ curl http://polar-dawn-5134.herokuapp.com/api?module=users -X POST -d '{"key":"val"}' -H 'accept:application/text'
-//auth $ curl http://user:wrongpass@polar-dawn-5134.herokuapp.com/auth?module=users -X POST -d '{"key":"val"}' -H 'accept:application/json'
+//api $ curl http://domain.com/api?module=users -X POST -d '{"key":"val"}' -H 'accept:application/text'
+//auth $ curl -u user:pass http://domain.com/auth?module=users -X POST -d '{"key":"val"}' -H 'accept:application/json'
 Api::error(function($message, $res){
     $res->status($message['status'])->json($message);
 });
