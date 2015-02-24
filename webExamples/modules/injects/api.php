c528dbc962d630bfc706d3dda4009d35d3081e83
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-02-24 15:44:06 +0000

    inject PUT example

--- webExamples/modules/injects/api.php
+++ webExamples/modules/injects/api.php
@@ -10,6 +10,14 @@ Api::post(function($req, $res, $injects){
     ]);
 });
 
+// curl -u user:pass http://domain.com/auth?module=injects -X PUT -d '{"job":"Security"}' -H 'accept:application/json'
+Api::put(function($req, $res, $injects){
+    $user = $injects['user'];
+    $user->job = $req->input('job'); // like $_POST
+    /* Save to DB maybe */
+    $res->json($user);
+});
+
 
 Api::error(function($message, $res){
     $res->status($message['status'])->json($message);
