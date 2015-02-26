d1fdac0cf24ade8d0e81bd0ee3807bcb8240ab74
Lee <trojan_spikepk@yahoo.co.uk>
2015-02-26 11:06:26 +0000

    check getallheaders on _server*

--- restExample/Modules/web/header.php
+++ restExample/Modules/web/header.php
@@ -3,7 +3,12 @@
 Api::auth(function($req, $res, $run, $injects){
     
     
-    $res->json( [ 'params' => $injects['params'], 'h-X-Forwarded-For' => $req->header('X-Forwarded-For'), 'h-all' => $req->header(), 'SERVER' => $_SERVER] );
+    $res->json( [ 
+        'params' => $injects['params'], 
+        'has-getAllHeader' => function_exists('getallheaders'),
+        'h-X-Forwarded-For' => $req->header('X-Forwarded-For'), 
+        'h-all' => $req->header(), 
+        'SERVER' => $_SERVER] );
     
 });
 
