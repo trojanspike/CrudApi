989a76d3d70d7a8107aa16d8055df0eca23874d1
Lee <trojan_spikepk@yahoo.co.uk>
2015-02-26 11:02:54 +0000

    check ->header X-Forwarded-For , return on servers with upper and lowercase chars

--- restExample/Modules/web/header.php
+++ restExample/Modules/web/header.php
@@ -3,7 +3,7 @@
 Api::auth(function($req, $res, $run, $injects){
     
     
-    $res->json( [ 'params' => $injects['params'], 'h-X-region' => $req->header('X-region'), 'h-all' => $req->header(), 'SERVER' => $_SERVER] );
+    $res->json( [ 'params' => $injects['params'], 'h-X-Forwarded-For' => $req->header('X-Forwarded-For'), 'h-all' => $req->header(), 'SERVER' => $_SERVER] );
     
 });
 
