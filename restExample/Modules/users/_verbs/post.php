df88a2871dacb4aabe5fc726875234512e73b348
Lee <trojan_spikepk@yahoo.co.uk>
2015-02-26 16:03:59 +0000

    add _file and verb to output

--- restExample/Modules/users/_verbs/post.php
+++ restExample/Modules/users/_verbs/post.php
@@ -2,6 +2,8 @@
 
 return function($req, $res, $injects){
     $res->json([
+        'file' => __FILE__,
+        'verb' => $req->verb,
         'get' => $req->get(),
         'input' => $req->input(),
         'header' => $req->header(),
