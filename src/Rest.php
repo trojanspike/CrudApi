795384cc19d05899466cd372201029768b77ca68
root <trojan_spikepk@yahoo.co.uk>
2015-02-27 11:59:40 -0500

    allow public $Dir pointing to rest directory

--- src/Rest.php
+++ src/Rest.php
@@ -2,21 +2,21 @@
 
 class Rest {
     
-    private static $_Policies, $_params, $_api, $restDir;
+    private static $_Policies, $_params, $_api;
+    public static $Dir;
     
-    public static function init($conf)
+    public static function init($parts)
     {
-        $parts = $conf['parts'];
-        static::$restDir = realpath($conf['restDir']);
+        static::$Dir = realpath(static::$Dir);
 
         static::$_api = $parts[0];
         unset($parts[0]);
         static::$_params = array_values($paths);
-        static::$_Policies = require_once static::$restDir.'/config/Policies.php';
+        static::$_Policies = require_once static::$Dir.'/config/Policies.php';
         
-        if( file_exists(static::$restDir.'/api/'.static::$_api.'.php') ){
-            require_once static::$restDir.'/api/'.static::$_api.'.php';
-            require_once static::$restDir.'/config/injects.php';
+        if( file_exists(static::$Dir.'/api/'.static::$_api.'.php') ){
+            require_once static::$Dir.'/api/'.static::$_api.'.php';
+            require_once static::$Dir.'/config/injects.php';
         } else {
             /*  Error */
             echo "error";
@@ -27,7 +27,7 @@ class Rest {
         if( ! in_array(static::$_api , array_keys(static::$_Policies)) )
         {
             /* Auth required by default */
-            require_once static::$restDir.'/config/Auth.php';
+            require_once static::$Dir.'/config/Auth.php';
             
         } else {
             static::_AuthChecker();
@@ -37,14 +37,14 @@ class Rest {
     private static function _AuthChecker()
     {
         $policy = static::$_Policies[static::$_api];
-        if( $policy == false ){
+        if( $policy === false ){
             /* No auth required */
-            require_once static::$restDir.'/config/noAuth.php';
+            require_once static::$Dir.'/config/noAuth.php';
         } else {
             if( is_array($policy) && in_array($_SERVER['REQUEST_METHOD'] , $policy) ){
-                require_once static::$restDir.'/config/noAuth.php';
+                require_once static::$Dir.'/config/noAuth.php';
             } else {
-                require_once static::$restDir.'/config/Auth.php';
+                require_once static::$Dir.'/config/Auth.php';
             }
         }
     }
