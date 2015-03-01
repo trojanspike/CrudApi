180493f6e08a1ab810ab7cd8c2438526ef0493c2
root <trojan_spikepk@yahoo.co.uk>
2015-03-01 11:43:16 -0500

    add outPut and contentType methods to Response class

--- src/lib/Response.php
+++ src/lib/Response.php
@@ -7,18 +7,37 @@ class Response {
 		$this->_status = 200;
 		$this->_content = 'application/javascript';
 	}
+	
+	
+	/* Setting methods */
+	
+	public function contentType($content){
+		$this->_content = $content;
+		return $this;
+	}
 
 	public function status($code){
 		$this->_status = (int)$code;
 		return $this;
 	}
+	
+	
+	/* output methods */
+	
 	public function json($obj){
-		/* 5.4* */		
+		/* default is 200 & application/javascript */
+		$this->render(json_encode($obj));
+	}
+	
+	public function outPut($content){
 		http_response_code($this->_status);
 		header('Content-Type: '.$this->_content);
-		echo json_encode($obj);
+		echo $content;
 		exit();
 	}
+	
+	
+	/* Quick herlper methods */
 
 	public function badRequest(){
 		$this->status(400)->json([
@@ -48,6 +67,17 @@ class Response {
 		$this->status(201)->json([
 			'message' => 'Success'
 		]);
+	}
+	
+	
+	
+	/* @start private */
+	private function render($content){
+		/* 5.4* */
+		http_response_code($this->_status);
+		header('Content-Type: '.$this->_content);
+		echo $content;
+		exit();
 	}
 
 }
