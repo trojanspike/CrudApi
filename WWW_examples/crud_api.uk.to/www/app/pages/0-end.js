22a7f34db3d2cf0c3241e7e350f63651ac232096
root <trojan_spikepk@yahoo.co.uk>
2015-03-07 07:14:25 -0500

    0.1.27 prep

--- WWW_examples/crud_api.uk.to/www/app/pages/0-end.js
+++
@@ -1,9 +0,0 @@
-window.angular.module('api')
-.config(['$urlRouterProvider', '$locationProvider', function($urlRouterProvider, $locationProvider){
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: true
-    });
-    $urlRouterProvider.otherwise('/docs/v1');
-    
-}]);
