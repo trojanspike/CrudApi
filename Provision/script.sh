c42aac32bafae5360146dfb2ebc621a6a0d57ff0
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-03-11 12:16:44 +0000

    add vagrant

---
+++ Provision/script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#http://stackoverflow.com/questions/7739645/install-mysql-on-ubuntu-without-password-prompt
+sudo apt-get update && sudo apt-get upgrade -y &&
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+sudo apt-get install php5 php5-cli php5-curl php5-mcrypt apache2 mysql-server redis-server php5-mysql php5-redis -yy &&
+
+mysql -u root -ppassword -e "CREATE DATABASE root_database;" &&
+sudo cp /vagrant/Provision/000-default.conf /etc/apache2/sites-enabled/ && service apache2 reload
+
+curl -sS https://getcomposer.org/installer | php &&
+mv composer.phar /usr/local/bin/composer && \
+
+cd /var/www/crud_api/web_interface/ && composer install && ./cmd schema up
