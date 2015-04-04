c6d26be21ce39cf634a0ff5bcb7532c7820bbcf6
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-04-04 17:15:39 +0100

    Illuminate setup DB connection

--- web_interface/core/Classes/Database/Illuminate.php
+++ web_interface/core/Classes/Database/Illuminate.php
@@ -6,6 +6,7 @@ use App\Config;
 /**
  * Extends Illuminate database class
  * @link http://laravel.com/api/4.0/Illuminate/Database/Query/Builder.html
+ * @link https://github.com/illuminate/database
  *
  * @copyright  28/03/15 , 16:28 lee
  * @license     MIT
@@ -14,38 +15,18 @@ use App\Config;
 
 class Illuminate extends Capsule {
 
-  protected static $instance = false;
-
     /**
      * Illuminate DB drive setup & config
      * 28/03/15 , 16:30
      *
-     * @throws Exception TODO - DbException
-     * @return void
+     * @return $capsule
      */
-    public function __construct()
-    {
-        $this->addConnection(Config::get('database')[Config::get('database.driver')]);
-        $this->setAsGlobal();
+    public function __construct(){
+        $capsule = new parent();
+        $capsule->addConnection(Config::get('database')[Config::get('database.driver')]);
+        $capsule->setAsGlobal();
     }
 
-    /**
-     * instance if the DB driver
-     * 28/03/15 , 16:30
-     *
-     * @return DB driver instance
-     */
-    public static function instance()
-    {
-      if( static::$instance === false )
-      {
-        return static::$instance = new Illuminate();
-      }
-      else
-      {
-        return static::$instance;
-      }
-    }
 
 }
 
