8bfb229f2f8b3bdd8cf7970d412a4edc0772092f
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-04-24 13:23:06 +0100

    mail default conf

---
+++ web_interface/core/Config/mail.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'mailgun' => [
+        'domain' => 'smtp.mailgun.org'
+        , 'port' => 465
+        , 'encrypt' => 'ssl'
+        , 'username' => 'postmaster@website.co.uk'
+        , 'password' => 'password'
+    ]
+    ,
+    'gmail' => [
+        'domain' => 'smtp.gmail.com'
+        , 'port' => 465
+        , 'encrypt' => 'ssl'
+        , 'username' => 'postmaster@website.co.uk'
+        , 'password' => 'password'
+    ]
+    , 'aws' => [
+        'domain' => 'email-smtp.eu-west-1.amazonaws.com'
+        , 'port' => 465
+        , 'encrypt' => 'ssl'
+        , 'username' => 'postmaster@website.co.uk'
+        , 'password' => 'password'
+    ]
+
+    , 'default' => 'mailgun'
+];
