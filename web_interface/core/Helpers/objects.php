d35c9c41b4b3c7df677a1f7cfd59a2bfb52587ca
trojanspike <trojan_spikepk@yahoo.co.uk>
2015-05-04 23:13:12 +0100

    test

---
+++ web_interface/core/Helpers/objects.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Does something interesting
+ * 28/03/15 , 16:30
+ * @param  string    $where  Where something interesting takes place
+ * @param  integer  $repeat How many times something interesting should happen
+ * @throws Exception If something interesting cannot happen
+ * @return Status
+ */
+
+if( ! function_exists('array2std') )
+{
+    function array2std( array $arr )
+    {
+        $stdClass = new StdClass;
+        foreach( $arr as $key => $val )
+        {
+            if( is_array( $val ) )
+            {
+                $stdClass->$key = array2std($val);
+            }
+            else
+            {
+                $stdClass->$key = $val;
+            }
+        }
+        return $stdClass;
+    }
+}
+
+/**
+ * Does something interesting
+ * 28/03/15 , 16:30
+ * @param  string    $where  Where something interesting takes place
+ * @param  integer  $repeat How many times something interesting should happen
+ * @throws Exception If something interesting cannot happen
+ * @return Status
+ */
+
+if( ! function_exists('std2array') )
+{
+    function std2array( stdClass $stdClass )
+    {
+    }
+}
+
+
+/**
+ * Does something interesting
+ * 28/03/15 , 16:30
+ * @param  string    $where  Where something interesting takes place
+ * @param  integer  $repeat How many times something interesting should happen
+ * @throws Exception If something interesting cannot happen
+ * @return Status
+ */
+
+if( ! function_exists('arrayFlatten') )
+{
+    function arrayFlatten( array $arr )
+    {
+    }
+}
+/*
+* arrayFlatten 
+return iterator_to_array( 
+	new RecursiveIteratorIterator(
+		new RecursiveArrayIterator( $array );
+	);
+ , false);
+*/
