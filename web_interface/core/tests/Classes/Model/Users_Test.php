0ff8313e93654461942db58dda892977dfaa81ac
trojanspike <v3047-github@yahoo.co.uk>
2015-10-29 11:20:27 +0000

    phpunit test file for Model directory

---
+++ web_interface/core/tests/Classes/Model/Users_Test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lee
+ * Date: 29/10/15
+ * Time: 11:19
+ */
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+class Users_Test extends \PHPUnit_Framework_TestCase {
+
+    private $param = false;
+
+    public function setUp()
+    {
+        $this->param = true;
+    }
+
+    public function tearDown()
+    {
+        $this->param = false;
+    }
+
+    public function testSetupPHPUnit()
+    {
+        $this->assertTrue( $this->param );
+
+    }
+
+}
