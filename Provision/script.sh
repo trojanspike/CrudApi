bd2d257a2a388a3191f71831e11bd8552dbc1945
trojanspike <v3047-github@yahoo.co.uk>
2015-11-03 15:47:53 +0000

    provision script chmod dirs

--- Provision/script.sh
+++ Provision/script.sh
@@ -12,30 +12,28 @@ MSQL_ROOT_PASS=password
 if [ ! -z $1 ];then
     MSQL_ROOT_PASS=$1
 fi
-echo $MSQL_ROOT_PASS;
 
 #http://stackoverflow.com/questions/7739645/install-mysql-on-ubuntu-without-password-prompt
 # http://phpdox.de/getting-started.html
 # http://www.dev-metal.com/install-setup-php-5-6-ubuntu-14-04-lts/
-sudo add-apt-repository ppa:ondrej/php5-5.6 <<EFO
+add-apt-repository ppa:ondrej/php5-5.6 <<EFO
 
 EFO
 
-sudo apt-get update && sudo apt-get upgrade -y && \
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MSQL_ROOT_PASS"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MSQL_ROOT_PASS"
-sudo apt-get install php5 php5-cli php5-curl php5-xsl php5-mcrypt apache2 mysql-server redis-server php5-mysql php5-redis -yy && \
-sudo php5enmod mcrypt curl
-
+apt-get update && apt-get upgrade -y && \
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MSQL_ROOT_PASS"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MSQL_ROOT_PASS"
+apt-get install php5 php5-cli php5-curl php5-xsl php5-mcrypt apache2 mysql-server redis-server php5-mysql php5-redis -yy && \
+php5enmod mcrypt curl && \
 mysql -u root -ppassword -e "CREATE DATABASE CrudApi_database;" && \
-sudo cp $BASE/Provision/000-default.conf /etc/apache2/sites-enabled/ && service apache2 reload
-
+rsync $BASE/Provision/000-default.conf /etc/apache2/sites-enabled/ && service apache2 reload && \
 curl -sS https://getcomposer.org/installer | php &&
 mv composer.phar /usr/local/bin/composer && \
 #
 cd /var/www/CrudApi/ && composer install && \
 wget https://phar.phpunit.de/phpunit.phar && \
 chmod +x phpunit.phar && \
-sudo mv phpunit.phar /usr/local/bin/phpunit && \
-wget http://phpdox.de/releases/phpdox.phar && sudo chmod +x phpdox.phar && \
-sudo mv phpdox.phar /usr/local/bin/phpdox
+mv phpunit.phar /usr/local/bin/phpunit && \
+wget http://phpdox.de/releases/phpdox.phar && chmod +x phpdox.phar && \
+mv phpdox.phar /usr/local/bin/phpdox && chown -R www-data:www-data $BASE/Application/www/ && \
+chown -R root:www-data $BASE/Application/core/Storage/ && chmod -R 770 $BASE/Application/core/Storage/
