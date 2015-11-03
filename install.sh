cfc939711694f91b4991d28a7549fb7a58e28f33
trojanspike <v3047-github@yahoo.co.uk>
2015-11-03 16:37:57 +0000

    install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ "$USER" != "root" ];then
+    echo "Please run script with root permissions"
+    exit 1
+fi
+
+echo -n "Please enter a password for mysql server's root account : "
+read pwd
+
+apt-get update && apt-get install git -y && \
+mkdir -p /var/www && cd $_ && \
+# git clone https://github.com/trojanspike/CrudApi.git && \
+/var/www/CrudApi/Provision/script.sh "$pwd"
