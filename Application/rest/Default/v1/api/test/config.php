8f248cb149b6c6bd19d9b9be794960b748f77952
trojanspike <v3047-github@yahoo.co.uk>
2015-11-03 15:47:24 +0000

    show more config

--- Application/rest/Default/v1/api/test/config.php
+++ Application/rest/Default/v1/api/test/config.php
@@ -4,6 +4,6 @@ use App\Config;
 
 Api::get(function($req, $res){
 
-    $res->json( array_merge( Config::get('mail') , Config::get('path') ) );
+    $res->json( array_merge_recursive( Config::get('mail') , Config::get('path'), Config::get('site'), Config::get('database') ) );
 
 });
